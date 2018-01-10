bd3a30d4f63ea274fbc89f39c5dbc9c46feca263
Shai Givony <sgivony@lifesize.com>
2018-01-10 04:18:21 -0600

    dynamic extension input added

--- server/app.js
+++ server/app.js
@@ -21,9 +21,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use('/api', index);
-app.get('*', (req, res) => {
-  res.sendFile('build/index.html', { root: global });
-});
+// app.get('*', (req, res) => {
+//   res.sendFile('build/index.html', { root: global });
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Caller from './Caller.js'
+
+var ReactDOM = require('react-dom');
 
 class App extends Component {
   constructor() {
     super();
+    this.handleSearch = this.handleSearch.bind(this);
+    this.searchExtension = this.searchExtension.bind(this);
     this.state = {
-      id: 'BLA',
+      id: 'Please Wait...',
       cookie: 'Cookie',
-      job: '000',
       caller: 'caller',
       callee: 'callee',
       callerExt: 'callerExt',
@@ -16,8 +20,8 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
-    fetch('/api/id', {
+  searchExtension(extension) {
+    fetch('/api/id/' + extension, {
       credentials: 'include'
     })
     .then(res => res.json())
@@ -31,30 +35,49 @@ class App extends Component {
     }.bind(this), 2000)
 
     setTimeout(function() { //Start the timer
-      fetch('/api/job/' + this.state.id)
+      fetch('/api/job/' + this.state.id + '/' + extension)
       //.then(res => console.log(res.cookie))
       .then(res => res.json())
       .then(json => this.setState({
-        job: json.messages[0].map.confid,
+        //job: json.messages[0].map.confid,
         caller: json.messages[0].map.from_displayname,
         callee: json.messages[0].map.to_displayname,
         callerExt: json.messages[0].map.from_extension,
         calleeExt: json.messages[0].map.to_extension,
-        //caller:
       }))
       .catch(err => console.log('err ' + err))
       .then(this.setState({ message: "bummer" }))
     }.bind(this), 3000)
   }
 
+  componentDidMount() {
+    //this.searchExtension(this.state.extensionFromUser);
+  }
+
+  handleSearch(evt) {
+    var ext = ReactDOM.findDOMNode(this.refs.extRef).value;
+    this.searchExtension(ext);
+  }
+
   render() {
     return (
-      <div className="App">
-        <div /*className="App-header"*/>
-          <h2>{this.state.id}</h2>
-          <h4>{this.state.job}</h4>
-          <div>Caller: {this.state.caller}  {this.state.callerExt}</div>
-          <div>Callee: {this.state.callee}  {this.state.calleeExt}</div>
+      <div className="canvas" id="canvas1">
+        <div id="callerExtension">
+          <input ref="extRef" type="text" />
+          <button className="btn btn-primary" onClick={this.handleSearch}>Search</button>
+        </div>
+        <div className="App">
+          <h2>Sumo Job ID: {this.state.id}</h2>
+          <Caller name={this.state.caller}
+                  client="Lifesize Web Client"
+                  extension={this.state.callerExt}
+                  ip="82.1.68.119"
+          />
+          <Caller name={this.state.callee}
+                  client="Lifesize Web Client"
+                  extension={this.state.calleeExt}
+                  ip="82.1.68.119"
+          />
         </div>
       </div>
     );
